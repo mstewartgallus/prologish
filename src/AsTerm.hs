b1d0203a602202c437209892c5636627bc3a4f89
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-09-15 19:55:40 -0700

    Remove removeVar messs

--- src/AsTerm.hs
+++ src/AsTerm.hs
@@ -63,11 +63,6 @@ instance Mal k => Category (Pf k) where
       me =
         V
           { out = out f . out g,
-            removeVar = \v -> case (removeVar f v, removeVar g v) of
-              (Just f', Just g') -> Just $ f' . (first &&& g')
-              (_, Just g') -> Just $ f . g'
-              (Just f', _) -> Just $ f' . (first &&& (g . second))
-              _ -> Nothing,
             removeLabel = \v -> case (removeLabel f v, removeLabel g v) of
               (Just f', Just g') -> Just $ (left ||| f') . g'
               (_, Just g') -> Just $ (left ||| (right . f)) . g'
@@ -93,9 +88,6 @@ instance Mal k => HasCoexp (Pf k) where
       me =
         V
           { out = mal $ out f,
-            removeVar = \v -> case removeVar f v of
-              Just f' -> Just $ swp (mal f')
-              _ -> Nothing,
             removeLabel = \v -> case removeLabel f v of
               Just f' -> Just $ mal (shuffleSum f')
               _ -> Nothing
@@ -105,19 +97,10 @@ instance Mal k => HasCoexp (Pf k) where
       me =
         V
           { out = try $ out f,
-            removeVar = \v -> case removeVar f v of
-              Just f' -> error "todo"
-              _ -> Nothing,
             removeLabel = \v -> case removeLabel f v of
               Just f' -> Just $ shuffleSum (try f')
               _ -> Nothing
           }
-
--- | Impossible for the same reasons
--- k c (a -> (v + b)) -> k c (v + (a -> b))
--- is impossible ...
-swp :: Mal k => k (a -< (v * b)) c -> k (v * (a -< b)) c
-swp _ = error "impossible"
 
 shuffleSum :: HasSum k => k b (a + (v + c)) -> k b (v + (a + c))
 shuffleSum x = ((right . left) ||| (left ||| (right . right))) . x
@@ -125,37 +108,17 @@ shuffleSum x = ((right . left) ||| (left ||| (right . right))) . x
 instance Mal k => Mal (Pf k) where
   u64 x = lift0 $ u64 x
 
-data Pf k env (b :: T) = V
-  { out :: k env b,
-    removeVar :: forall v. Var v -> Maybe (Pf k (AsObject v * env) b),
-    removeLabel :: forall v. Label v -> Maybe (Pf k env (AsObject v + b))
+data Pf k (a :: T) (b :: T) = V
+  { out :: k a b,
+    removeLabel :: forall v. Label v -> Maybe (Pf k a (AsObject v + b))
   }
 
-data Var a = Var (Type.ST a) Id
-
 data Label a = Label (Type.ST a) Id
-
-eqVar :: Var a -> Var b -> Maybe (a :~: b)
-eqVar (Var t m) (Var t' n)
-  | m == n = Type.eqT t t'
-  | otherwise = Nothing
 
 eqLabel :: Label a -> Label b -> Maybe (a :~: b)
 eqLabel (Label t m) (Label t' n)
   | m == n = Type.eqT t t'
   | otherwise = Nothing
-
-mkVar :: HasProduct k => Var a -> Pf k x (AsObject a)
-mkVar v@(Var _ n) = me
-  where
-    me =
-      V
-        { out = error ("free variable " ++ show n),
-          removeVar = \maybeV -> case eqVar v maybeV of
-            Nothing -> Nothing
-            Just Refl -> Just (lift0 first),
-          removeLabel = const Nothing
-        }
 
 mkLabel :: HasSum k => Label a -> Pf k (AsObject a) x
 mkLabel v@(Label _ n) = me
@@ -165,8 +128,7 @@ mkLabel v@(Label _ n) = me
         { out = error ("free label " ++ show n),
           removeLabel = \maybeV -> case eqLabel v maybeV of
             Nothing -> Nothing
-            Just Refl -> Just (lift0 left),
-          removeVar = const Nothing
+            Just Refl -> Just (lift0 left)
         }
 
 lift0 :: k a b -> Pf k a b
@@ -175,8 +137,7 @@ lift0 x = me
     me =
       V
         { out = x,
-          removeLabel = const Nothing,
-          removeVar = const Nothing
+          removeLabel = const Nothing
         }
 
 lift1 ::
@@ -188,10 +149,7 @@ lift1 f x = me
   where
     me =
       V
-        { out = f (out x),
-          removeVar = \v -> case removeVar x v of
-            Just x' -> Just $ lift1 f x'
-            _ -> Nothing
+        { out = f (out x)
         }
 
 lift2 ::
@@ -209,11 +167,6 @@ lift2 f x y = me
             (Just x', Just y') -> error "foo"
             (_, Just y') -> error "foo"
             (Just x', _) -> error "foo"
-            _ -> Nothing,
-          removeVar = \v -> case (removeVar x v, removeVar y v) of
-            (Just x', Just y') -> Just $ lift2 f x' y'
-            (_, Just y') -> Just $ lift2 f (x . second) y'
-            (Just x', _) -> Just $ lift2 f x' (y . second)
             _ -> Nothing
         }
 
@@ -228,11 +181,6 @@ colift2 f x y = me
     me =
       V
         { out = f (out x) (out y),
-          removeVar = \v -> case (removeVar x v, removeVar y v) of
-            (Just x', Just y') -> error "todo"
-            (Just x', _) -> error "todo"
-            (_, Just y') -> error "todo"
-            _ -> Nothing,
           removeLabel = \v -> case (removeLabel x v, removeLabel y v) of
             (Just x', Just y') -> Just $ colift2 f x' y'
             (_, Just y') -> error "foo"
